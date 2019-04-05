b8f54b9aba808241b8ceebc32f3aca82369677d9
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-05 23:48:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++);
-  if (i === 1); {
-  array.push("I am 1 strange loop.");
-  return array
+  if (i === 1); 
+  array.push("I am 1 strange loop.") {
 }else
-  return ` I am ${i} strange loops.`
+  array.push(` I am ${i} strange loops.`) {
+    
+  }
 }
